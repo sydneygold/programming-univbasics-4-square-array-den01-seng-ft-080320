37da3a2b1325143b378cc70c51cda9f656f917c3
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-16 22:12:48 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,9 @@
 def square_array(numbers)
   counter = 0
   final_array = []
-while counter < numbers.length 
+while counter < numbers.length
   final_array << numbers[counter] * number[counter]
   counter += 1
-end  
+end
 return final_array
 end
-
